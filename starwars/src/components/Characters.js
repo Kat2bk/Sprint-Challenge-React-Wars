d2aeed1e06ebc9d6a92594f8170556950e1625c5
unknown <Kat2bk@gmail.com>
2020-01-24 10:56:05 -0800

    styled more of the grid, added a glowy effect, want to work on font title

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,22 +1,32 @@
 import React from "react";
-import { Card, CardTitle } from 'reactstrap';
-import styled from 'styled-components'
+import { Card, CardTitle } from "reactstrap";
+import styled from "styled-components";
 
 const CharDiv = styled.div`
-margin: 2%;
+margin: 5%;
 background-color: rgba(168, 107, 50, 0.3);
 width: 300px;
 border: 1px solid brown;
 border-radius: 10px;
-`
+padding: 1%;
+text-align: center;
+box-shadow:  inset 0 0 50px #fff,      
+inset 10px 0 80px #0064D1,  
+inset -10px 0 80px #0ff,  
+inset 10px 0 300px #3287E3,  
+inset -20px 0 300px #2D3CE2, 
+0 0 50px #fff,           
+-10px 0 80px #6DA7E6,     
+10px 0 80px #0ff; 
+`;
 
 export default function Characters(props) {
   return (
     <CharDiv>
-       <h2>Name: {props.post.name}</h2>
-       <h2> Height: {props.post.height}</h2>
-       <h2>Gender: {props.post.gender}</h2>
-       <h2>Date of Birth: {props.post.birth_year}</h2>
+      <h2>Name: {props.post.name}</h2>
+      <h2> Height: {props.post.height}</h2>
+      <h2>Gender: {props.post.gender}</h2>
+      <h2>Date of Birth: {props.post.birth_year}</h2>
     </CharDiv>
   );
 }
